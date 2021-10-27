0d634850defdc493f402a7de71fc97c5bfd1ff4c
Marc-Antoine BROHAN <mabrohan@live.fr>
2021-10-27 10:50:27 +0200

    inport / export

--- front/js/functions.js
+++ front/js/functions.js
@@ -1,3 +1,8 @@
+const url = "http://localhost:3000/api/products/";
+const idKanap = new URL(location.href).searchParams.get('id');
+const urlKanap = url + idKanap;
+const kanapsInLocalStorage = JSON.parse(localStorage.getItem("kanapsToOrder"));
+
 export function getArticles(){
     return fetch(url)
         .then(function(response){
