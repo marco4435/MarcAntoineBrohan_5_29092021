3acebcfc7a5f25dc41ea8a8307be9b4ac9cc2966
Marc-Antoine BROHAN <mabrohan@live.fr>
2021-10-27 10:59:19 +0200

    inport / export

--- front/js/const.js
+++ front/js/const.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/api/products/";
-const idKanap = new URL(location.href).searchParams.get('id');
-const urlKanap = url + idKanap;
-const kanapsInLocalStorage = JSON.parse(localStorage.getItem("kanapsToOrder"));
+export const url = "http://localhost:3000/api/products/";
+export const idKanap = new URL(location.href).searchParams.get('id');
+export const urlKanap = url + idKanap;
+export const kanapsInLocalStorage = JSON.parse(localStorage.getItem("kanapsToOrder"));
--- front/js/functions.js
+++ front/js/functions.js
@@ -1,7 +1,4 @@
-const url = "http://localhost:3000/api/products/";
-const idKanap = new URL(location.href).searchParams.get('id');
-const urlKanap = url + idKanap;
-const kanapsInLocalStorage = JSON.parse(localStorage.getItem("kanapsToOrder"));
+import {url, idKanap, urlKanap, kanapsInLocalStorage} from "./const.js";
 
 export function getArticles(){
     return fetch(url)
