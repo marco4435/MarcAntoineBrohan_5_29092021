fa98a76f9043b6e0d68c57352b1eb10b13ded143
Marc-Antoine BROHAN <mabrohan@live.fr>
2021-10-28 14:44:26 +0200

    comments

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,16 +4,21 @@ import {displayCarts, getTotalQuantity, displayTotalQuantity, getTotalPrice, dis
 main();
 
 async function main(){
-    // Execution of the following functions if the localstorage contains at least 1 item. - Execution des fonctions suivantes si le localstorage contient au moins 1 élément.
+    // Execution of the following functions if the localstorage contains at least 1 item. - Éxecution des fonctions suivantes si le localstorage contient au moins 1 élément.
     if(kanapsInLocalStorage){
-        // Cart display. - Affichage du panier.
+        // Shopping cart display. - Affichage du panier.
         displayCarts();
+        // Shopping cart articles total quantity calculation and display. - Calcul et affichage de la quantité d'articles présents dans le panier.
         const TotalQuantity = await getTotalQuantity();
         displayTotalQuantity(TotalQuantity);
+        // Shopping cart articles total price calculation and display. - Calcul et affichage du prix total des articles présents dans le panier.
         const TotalPrice = await getTotalPrice();
         displayTotalPrice(TotalPrice);
+        // Delete article function. - Fonction de retrait d'un article.
         deleteKanap();
+        // Quantity per article modification function. - Fonction de modification de la quantité d'un article.
         changeQuantity();
+        // Command validation function. - Fonction de validation de la commande.
         sendOrder();
     }
 }
