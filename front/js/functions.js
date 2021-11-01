83926261eb95455b947020fcc62189f82d12565c
Marc-Antoine BROHAN <mabrohan@live.fr>
2021-11-01 08:52:38 +0100

    function validation

--- front/js/functions.js
+++ front/js/functions.js
@@ -286,43 +286,72 @@ export function sendOrder(){
 }
 
 export function validation(contact){
-    test(contact);
-    if(test(contact)){
+    firstNameIsValid(contact);
+    lastNameIsValid(contact);
+    addressIsValid(contact);
+    cityIsValid(contact);
+    emailIsValid(contact);
+
+    if (firstNameIsValid(contact) && lastNameIsValid(contact) && addressIsValid(contact) && cityIsValid(contact) && emailIsValid(contact)){
         return true;
     } 
-    else{
+    else {
         alert('Le formulaire contient des erreurs.');
         return false;
     }
 }
 
-export function test(contact){
+export function firstNameIsValid(contact){
     let firstNameRegex = contact.firstName;
     let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
-    let lastNameRegex = contact.lastName;
-    let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
-    let addressRegex = contact.address;
-    let addressErrorMsg = document.getElementById("addressErrorMsg");
-    let cityRegex = contact.city;
-    let cityErrorMsg = document.getElementById("cityErrorMsg");
-    const emailRegex = contact.email;
-    const emailErrorMsg = document.getElementById("emailErrorMsg");
-    if(/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(firstNameRegex)){
-        if(/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(lastNameRegex)){
-            if(/^[-'a-zA-Z0-9À-ÖØ-öø-ÿ\s]{3,}$/.test(addressRegex)){
-                if(/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(cityRegex)){
-                    if(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test(emailRegex)){
-                        return true;
-                    }
-                    return true;
-                }
-                return true;
-            }
-            return true;
-        }
+    if (/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(firstNameRegex)){
         return true;
     } 
     else {
+        return false;
+    }
+}
+
+export function lastNameIsValid(contact){
+    let lastNameRegex = contact.lastName;
+    let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+    if(/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(lastNameRegex)){
+        return true;
+    } 
+    else{
+        return false;
+    }
+}
+
+export function addressIsValid(contact){
+    let addressRegex = contact.address;
+    let addressErrorMsg = document.getElementById("addressErrorMsg");
+    if(/^[-'a-zA-Z0-9À-ÖØ-öø-ÿ\s]{3,}$/.test(addressRegex)){
+        return true;
+    } 
+    else{
+        return false;
+    }
+}
+
+export function cityIsValid(contact){
+    let cityRegex = contact.city;
+    let cityErrorMsg = document.getElementById("cityErrorMsg");
+    if(/^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{3,}$/.test(cityRegex)){
+        return true;
+    } 
+    else{
+        return false;
+    }
+}
+
+export function emailIsValid(contact){
+    const emailRegex = contact.email;
+    const emailErrorMsg = document.getElementById("emailErrorMsg");
+    if(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test(emailRegex)){
+        return true;
+    } 
+    else{
         return false;
     }
 }
